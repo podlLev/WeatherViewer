385188beaa87a75757da415f0248cac7095a9a82
podlLev <pidlisnyilev@gmail.com>
2026-06-05 19:08:48 +0300

    feat: add LocationRepository with dependencies

---
+++ src/main/java/com/weatherviewer/repository/LocationRepository.java
@@ -0,0 +1,12 @@
+package com.weatherviewer.repository;
+
+import com.weatherviewer.model.Location;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface LocationRepository extends JpaRepository<Location, UUID> {
+
+}
