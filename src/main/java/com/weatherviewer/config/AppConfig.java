a4512a0c2ac15506dfef52bfdbeed9d56d23e1af
podlLev <pidlisnyilev@gmail.com>
2026-06-07 13:48:55 +0300

    feat: enable secure form authentication and user session management

--- src/main/java/com/weatherviewer/config/AppConfig.java
+++ src/main/java/com/weatherviewer/config/AppConfig.java
@@ -2,6 +2,8 @@ package com.weatherviewer.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
@@ -14,6 +16,11 @@ public class AppConfig {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public RedirectStrategy redirectStrategy() {
+        return new DefaultRedirectStrategy();
     }
 
     @Bean
