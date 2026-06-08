1e39b996509ee0a0007bb83298725273e59752db
podlLev <pidlisnyilev@gmail.com>
2026-06-08 14:45:16 +0300

    refactor: simplify location deletion using repository methods and remove manual cascade handling

--- src/main/java/com/weatherviewer/security/CustomAuthFailureHandler.java
+++ src/main/java/com/weatherviewer/security/CustomAuthFailureHandler.java
@@ -1,15 +1,7 @@
 package com.weatherviewer.security;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.web.RedirectStrategy;
-import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
-
-import java.io.IOException;
 
 @Component
 public class CustomAuthFailureHandler extends SimpleUrlAuthenticationFailureHandler {
