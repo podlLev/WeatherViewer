fbb7a40626421d89b6050d039fdc07e4dd08f104
podlLev <pidlisnyilev@gmail.com>
2026-06-09 12:44:06 +0300

    feat: add validation annotations

---
+++ src/main/java/com/weatherviewer/validation/annotation/Password.java
@@ -0,0 +1,21 @@
+package com.weatherviewer.validation.annotation;
+
+import com.weatherviewer.validation.validator.PasswordValidator;
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.*;
+
+@Documented
+@Constraint(validatedBy = PasswordValidator.class)
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Password {
+
+    String message() default "Password must be at least 8 characters long and include a mix of letters, numbers, and symbols.";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+}
---
+++ src/main/java/com/weatherviewer/validation/annotation/PasswordMatches.java
@@ -0,0 +1,21 @@
+package com.weatherviewer.validation.annotation;
+
+import com.weatherviewer.validation.validator.PasswordMatchesValidator;
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.*;
+
+@Documented
+@Constraint(validatedBy = PasswordMatchesValidator.class)
+@Target({ ElementType.TYPE })
+@Retention(RetentionPolicy.RUNTIME)
+public @interface PasswordMatches {
+
+    String message() default "Passwords do not match";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+}
