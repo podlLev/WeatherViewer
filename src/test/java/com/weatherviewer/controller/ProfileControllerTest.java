447174338edab425d7be96916cd03bb1bbb6b14e
podlLev <pidlisnyilev@gmail.com>
2026-08-04 10:00:58 +0300

    test: add controller integration tests

--- src/test/java/com/weatherviewer/controller/ProfileControllerTest.java
+++ src/test/java/com/weatherviewer/controller/ProfileControllerTest.java
@@ -6,6 +6,7 @@ import com.weatherviewer.model.enums.UnitSystem;
 import com.weatherviewer.model.enums.UserStatus;
 import com.weatherviewer.security.SecUser;
 import com.weatherviewer.service.UserService;
+import com.weatherviewer.testcontainers.TestcontainersConfiguration;
 import com.weatherviewer.validation.validator.PasswordMatchesValidator;
 import com.weatherviewer.validation.validator.UniqueEmailValidator;
 import org.junit.jupiter.api.BeforeEach;
@@ -13,6 +14,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -30,6 +32,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@Import(TestcontainersConfiguration.class)
 class ProfileControllerTest {
 
     @Autowired
