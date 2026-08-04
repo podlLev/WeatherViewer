2d3a2a46c196c454eee40251426060bc10784c31
podlLev <pidlisnyilev@gmail.com>
2026-08-04 10:01:14 +0300

    test: add repository integration tests

--- src/test/java/com/weatherviewer/repository/LocationRepositoryTest.java
+++ src/test/java/com/weatherviewer/repository/LocationRepositoryTest.java
@@ -4,17 +4,20 @@ import com.weatherviewer.model.Location;
 import com.weatherviewer.model.User;
 import com.weatherviewer.model.enums.Role;
 import com.weatherviewer.model.enums.UserStatus;
+import com.weatherviewer.testcontainers.TestcontainersConfiguration;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.context.annotation.Import;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
+@Import(TestcontainersConfiguration.class)
 class LocationRepositoryTest {
 
     @Autowired
